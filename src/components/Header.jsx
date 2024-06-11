6bc52b5f147cabef694a4edf3746215ac6470cde
Alena <alyonatihomirova9@gmail.com>
2024-06-11 16:20:43 +0300

    Update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ import Logo from "../assets/logo.png"
 function Header() {
     return (
         <>
-            <img src={Logo} className="logo" alt="Logo"></img>
+            {/* <img src={Logo} className="logo" alt="Logo"></img> */}
+            <i className="logo fa-solid fa-mountain-sun"></i>
             <h1 className="header-title">Travel Wishboard</h1>
         </>
     )
