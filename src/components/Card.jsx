d119d768686c41fc1e1e112988d23578eb8c4923
Alena <alyonatihomirova9@gmail.com>
2024-06-11 15:59:51 +0300

    Update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
 import React from "react"
-import SpringNewZealand from "../assets/Spring-new-zealand.png"
+import SpringNewZealand from "../assets/spring-new-zealand.png"
 import LocationPin from "../assets/location-pin.png"
 
 function Card(props) {
     return (
         <div className="card">
             <div className="card-image-container">
-                <img src={props.item.imageUrl} className="card-image"></img>
+                <img src={props.item.imageUrl} className="card-image" alt={props.item.alt}></img>
                 {/* <img src={SpringNewZealand} className="card-image"></img> */}
             </div>
             <div className="card-info">
                 <div className="card-location-container">
                     <div className="card-location-country">
-                        <img src={LocationPin} className="location-pin" alt="Location pin"></img>
-                        <p className="card-location">{props.item.location}</p>
+                        {/* <img src={LocationPin} className="location-pin" alt="Location pin"></img> */}
+                        <i className="location-pin fa-solid fa-location-dot"></i>
+                        <h4 className="card-location">{props.item.location}</h4>
                     </div>
                     {/* <a href={props.item.googleMapsUrl}>View on Google Maps</a> */}
                 </div>
--- src/data.js
+++ src/data.js
@@ -1,4 +1,9 @@
-import springNewZealand from "./assets/Spring-new-zealand.png"
+import springNewZealand from "./assets/spring-new-zealand.png"
+import milfordSound from "./assets/milford-sound.png"
+import eiffelTower from "./assets/eiffel-tower.png"
+import stonehenge from "./assets/stonehenge.png"
+import greatBarrierReef from "./assets/great-barrier-reef.png"
+
 
 const Data = [
     // {
@@ -17,28 +22,32 @@ const Data = [
         title: "Milford Sound",
         location: "New Zealand",
         description: "Milford Sound is a fiord in the southwest of New Zealand’s South Island, within Fiordland National Park. Known for its towering Mitre Peak, rainforests, and waterfalls like Stirling and Bowen falls, it's one of the most beautiful places in the world and a top tourist destination.",
-        imageUrl: springNewZealand
+        imageUrl: milfordSound,
+        alt: "Milford Sound"
     },
     {
         id: 2,
         title: "Eiffel Tower",
         location: "France",
         description: "The Eiffel Tower is an iconic wrought-iron lattice tower on the Champ de Mars in Paris, France. Standing at 324 meters (1,063 feet) tall, it is one of the most recognizable structures in the world and a global cultural icon of France.",
-        imageUrl: springNewZealand
+        imageUrl: eiffelTower,
+        alt: "Eiffel Tower"
     },
     {
         id: 3,
         title: "Stonehenge",
         location: "United Kingdom",
         description: "Stonehenge is a prehistoric monument in Wiltshire, England. One of the most famous landmarks in the UK, it consists of a ring of standing stones, each around 4 meters high, 2.1 meters wide, and weighing approximately 25 tons. Its exact purpose remains a mystery.",
-        imageUrl: springNewZealand
+        imageUrl: stonehenge,
+        alt: "Stonehenge"
     },
     {
         id: 4,
         title: "Great Barrier Reef",
         location: "Australia",
         description: "The Great Barrier Reef is the world’s largest coral reef system, located in the Coral Sea, off the coast of Queensland, Australia. It is composed of over 2,900 individual reefs and 900 islands stretching over 2,300 kilometers (1,400 miles). The reef is renowned for its breathtaking marine biodiversity and is a UNESCO World Heritage Site.",
-        imageUrl: springNewZealand
+        imageUrl: greatBarrierReef,
+        alt: "Great Barrier Reef"
       }
       
 ]
